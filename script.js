269ef11364e5052c7d2ea517f88fad281137bc9c
RealityMHG <rafaelrego2000@gmail.com>
2024-06-18 15:42:01 +0100

    finished project within odin parameters

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
 const container  = document.querySelector(".container");
+
+//Height of the Container
 const HEIGHT = 450;
 
+//Default Rows and Columns
 let currentRow = 16;
 let currentColumn = 16;
 
+//initialize the sketch
+sketchBegin(currentRow,currentColumn);
+
+//Reset button with the same grid
 let resetButton = document.querySelector(".reset-btn");
 resetButton.addEventListener("click", function() {
-    location.reload();
+    sketchClear();
+    sketchBegin(currentRow,currentColumn);
 });
 
+//Reset button but with prompt for different grid
 let gridChangeButton = document.querySelector(".grid-btn");
 gridChangeButton.addEventListener("click", function() {
+    sketchClear();
     let gridPromptRow = Number(window.prompt("How many rows?", 16));
+    while(gridPromptRow >= 100 || gridPromptRow <= 0) {
+        gridPromptRow = Number(window.prompt("Choose between 1 and 100...", 16));
+    }
     currentRow = gridPromptRow;
+
     let gridPromptColumn = Number(window.prompt("How many columns?", 16));
+    while(gridPromptColumn >= 100 || gridPromptColumn <= 0) {
+        gridPromptColumn = Number(window.prompt("Choose between 1 and 100...", 16));
+    }
     currentColumn = gridPromptColumn;
     sketchBegin(currentRow,currentColumn);
 });
 
-sketchBegin(currentRow,currentColumn);
-
+//Clear the rows and columns
 function sketchClear() {
-    
+    document.querySelectorAll(".column").forEach(e => e.remove());
+    document.querySelectorAll(".row").forEach(e => e.remove());
 }
 
+//Create the rows and columns
 function sketchBegin(rows,columns) {
     //Create Rows
-    for (let i=0; i<=rows-1; i++) {
+    for (let i=0; i<rows; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
 
         //Create Columns
-        for(let o=0; o<=columns-1; o++) {
+        for(let o=0; o<columns; o++) {
             let column = document.createElement("div");
             column.classList.add("column");
 
